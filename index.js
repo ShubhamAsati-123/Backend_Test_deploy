544e26ef922a839f379c4a2ced9a9a8982b7aa44
JokerSA <asatishubham2004@gmail.com>
2023-10-31 15:11:52 +0530

    post request added

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ import cors from "cors";
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-app.use(cors()); 
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/submit", (req, res) => {
+  const { data } = req.body;
+
+  console.log("Received data:", data);
+
+  res.json({ message: "Data received successfully", receivedData: data });
 });
 
 app.listen(port, () => {
